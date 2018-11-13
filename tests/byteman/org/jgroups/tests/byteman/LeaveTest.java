7dde7775090de1597148e89fa2cf537c19c25311
Bela Ban <belaban@yahoo.com>
2018-11-13 16:18:53 +0100

    - Moved reproducer ConcurrentLeaversTest to byteman/LeaveTest

--- tests/byteman/org/jgroups/tests/byteman/LeaveTest.java
+++ tests/byteman/org/jgroups/tests/byteman/LeaveTest.java
@@ -1,4 +1,4 @@
-package org.jgroups.tests;
+package org.jgroups.tests.byteman;
 
 import org.jgroups.Global;
 import org.jgroups.JChannel;
@@ -15,16 +15,17 @@ import java.net.InetAddress;
 import java.util.stream.Stream;
 
 /**
- * Tests gracefully and concurrently leaving coordinator and a successor coordinator.
+ * Tests graceful leaves of multiple members, especially coord and next-in-line.
  * Nodes are leaving gracefully so no merging is expected.<br/
  * Reproducer for https://issues.jboss.org/browse/JGRP-2293.
  *
  * @author Radoslav Husar
+ * @author Bela Ban
  */
 @Test(groups = Global.FUNCTIONAL, singleThreaded = true)
-public class ConcurrentLeaversTest {
+public class LeaveTest {
 
-    protected static final int NUM = 10;
+    protected static final int NUM = 4;
     protected static final int NUM_LEAVERS = 2;
     protected static final InetAddress LOOPBACK;
 
@@ -41,20 +42,49 @@ public class ConcurrentLeaversTest {
     @BeforeMethod
     protected void setup() throws Exception {
         for (int i = 0; i < channels.length; i++) {
-            channels[i] = create(String.valueOf(i + 1)).connect(ConcurrentLeaversTest.class.getSimpleName());
-            Util.sleep(i < NUM_LEAVERS ? 2000 : 100);
+            channels[i] = create(String.valueOf(i + 1)).connect(LeaveTest.class.getSimpleName());
+            Util.sleep(i < 1 ? 2000 : 100);
         }
         Util.waitUntilAllChannelsHaveSameView(10000, 1000, channels);
     }
 
     @AfterMethod
     protected void destroy() {
-        for (int i = channels.length - 2; i >= 0; i--)
-            channels[i].close();
+        Util.closeReverse(channels);
     }
 
+    /** A single member (coord) leaves */
+    public void testLeaveOfSingleCoord() throws Exception {
+        JChannel x=null;
+        destroy();
+        try {
+            x=create("X").connect("x-cluster");
+            assert x.getView().size() == 1;
+            Util.close(x);
+            assert x.getView() == null;
+        }
+        finally {
+            Util.close(x);
+        }
+    }
 
-    public void testGracefulLeave() throws Exception {
+    /** The coord leaves */
+    public void testCoordLeave() {
+        Util.close(channels[0]);
+        Stream.of(1,2,3).map(i -> channels[i])
+          .peek(ch -> System.out.printf("%s: %s\n", ch.getAddress(), ch.getView()))
+          .allMatch(ch -> ch.getView().size() == 3 && ch.getView().getCoord().equals(channels[1].getAddress()));
+    }
+
+    /** A participant leaves */
+    public void testParticipantLeave() {
+        Util.close(channels[2]);
+        Stream.of(0,1,3).map(i -> channels[i])
+          .peek(ch -> System.out.printf("%s: %s\n", ch.getAddress(), ch.getView()))
+          .allMatch(ch -> ch.getView().size() == 3 && ch.getView().getCoord().equals(channels[0].getAddress()));
+    }
+
+    public void testConcurrentLeaves() throws Exception {
         System.out.println(Util.printViews(channels));
         System.out.println("\n");
 
